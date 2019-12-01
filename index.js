ccc9fc465ab42ada1a143cc36b6b2935ad5f28d4
wzc <newwangzhicheng@hotmail.com>
2019-12-01 23:20:25 +0800

    fixed a bug: tips is invaid when init an inexistant template

--- index.js
+++ index.js
@@ -18,43 +18,51 @@ const templates ={
 program.version('1.0.0', '-v, --version')
     .command('init <projName> <tempName>')
     .action((projName, tempName) => {
-        let downlodaUrl;
-        if(tempName){
-            downlodaUrl = templates[tempName].downloadUrl;
-        } else {
-            downloadUrl = templates['d'].downloadUrl;
-        }
-        if(!fs.existsSync(projName)){
-            inquirer.prompt([
-                {
-                    name: 'description',
-                    message: '请输入项目描述'
-                }
-            ]).then((answers) => {
-                const spinner = ora('正在下载模板...').start();
-                download(downlodaUrl, projName, {clone: true}, (err) => {
-                    if(err){
-                        spinner.fail();
-                        console.log(symbols.error, chalk.red(err));
-                    }else{
-                        spinner.succeed();
-                        const fileName = `${projName}/package.json`;
-                        const meta = {
-                            projName,
-                            description: answers.description,
-                        }
-                        if(fs.existsSync(fileName)){
-                            const content = fs.readFileSync(fileName).toString();
-                            const result = handlebars.compile(content)(meta);
-                            fs.writeFileSync(fileName, result);
-                        }
-                        console.log(symbols.success, chalk.green('项目初始化完成'));
-                    }
-                })
-            })
-        }else{
-            // 错误提示项目已存在，避免覆盖原有项目
-            console.log(symbols.error, chalk.red('项目已存在'));
-        }
-    })
+        init(projName, tempName);
+    });
 program.parse(process.argv);
+
+function init(projName, tempName){
+    let downloadUrl;
+    if(tempName == "d" || tempName == "default"){
+        downloadUrl = templates['d'].downloadUrl;
+        console.log(downloadUrl);
+    } else if(templates[tempName] !== undefined){
+        downloadUrl = templates[tempName].downloadUrl;
+    } else {
+        console.log("模板不存在（默认d, default）");
+        return;
+    }
+    if(!fs.existsSync(projName)){
+        inquirer.prompt([
+            {
+                name: 'description',
+                message: '请输入项目描述'
+            }
+        ]).then((answers) => {
+            const spinner = ora('正在下载模板...').start();
+            download(downloadUrl, projName, {clone: true}, (err) => {
+                if(err){
+                    console.log(symbols.error, chalk.red(err));
+                    spinner.fail("下载失败");
+                }else{
+                    spinner.succeed();
+                    const fileName = `${projName}/package.json`;
+                    const meta = {
+                        projName,
+                        description: answers.description,
+                    }
+                    if(fs.existsSync(fileName)){
+                        const content = fs.readFileSync(fileName).toString();
+                        const result = handlebars.compile(content)(meta);
+                        fs.writeFileSync(fileName, result);
+                    }
+                    console.log(symbols.success, chalk.green('项目初始化完成'));
+                }
+            })
+        })
+    }else{
+        // 错误提示 项目已存在，避免覆盖原有项目
+        console.log(symbols.error, chalk.red('项目已存在'));
+    }
+}
